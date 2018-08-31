22ad1bb35063fd9c07b857e99e72d2d1fe145b1e
Nampyo Jeong <jungnampyo@gmail.com>
2018-08-31 10:43:51 +0900

    App title 변경

--- lib/ui/MainWidget.dart
+++ lib/ui/MainWidget.dart
@@ -38,7 +38,7 @@ class MainState extends State<MainWidget> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text('Eos Node Checker'),
+        title: Text('EOS Node Monitor'),
       ),
       body: buildNodes(),
     );
